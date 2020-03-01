d1499637b2435c379586144e260e3064eb8c9c96
Paul Fletcher-Hill <pfletcherhill@gmail.com>
2020-02-29 19:28:21 -0800

    Separate out examples for easier learning

---
+++ examples/attackEnemyHQ.ts
@@ -0,0 +1,47 @@
+require("dotenv").config();
+import { Fighter, HQ, AttackCommand } from "@meka-js/core";
+import { MekaClient } from "@meka-js/client";
+
+// Declare Meka config using environment variables
+const mekaConfig = {
+  gameId: process.env.GAME_ID || "",
+  apiUrl: process.env.MEKA_API_URL || "http://localhost:3000",
+  webSocketUrl: process.env.MEKA_WEB_SOCKET_URL || "ws://localhost:3000",
+  apiKey: process.env.MEKA_API_KEY || "",
+  apiSecret: process.env.MEKA_API_SECRET || ""
+};
+
+const attackEnemyHQ = (meka: MekaClient, fighter: Fighter, hq: HQ) => {
+  if (meka.unitIsBusy(fighter)) return;
+  const command = new AttackCommand({
+    unit: fighter,
+    args: { targetId: hq.id }
+  });
+  meka.sendCommand(command);
+  console.log("Sent command", command);
+};
+
+// Print out the turn every tick
+const doTick = (meka: MekaClient, teamId: string) => {
+  const team = meka.game.getTeam(teamId);
+  const enemyTeam = meka.game.teams.find(t => t.id !== teamId);
+  const enemyHq = enemyTeam.hq;
+  console.log("Enemy HQ health: ", enemyHq.hp);
+  if (!team.fighters.length) {
+    console.log("You don't have any fighters to command!");
+    return;
+  }
+  team.fighters.forEach(fighter => attackEnemyHQ(meka, fighter, enemyHq));
+};
+
+// Define meka object and set up tick event handler
+const main = async () => {
+  const meka = new MekaClient(mekaConfig);
+  await meka.connect();
+  const me = await meka.api.me();
+  const teamId = me.uid;
+  meka.on("tick", () => doTick(meka, teamId));
+};
+
+// Call main function
+main();
